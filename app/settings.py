87e0d90453ecd7acbe965c3518f26025d3a57f19
charity-bit <charitynyanchera@gmail.com>
2022-06-17 03:59:46 +0300

    deployement to heroku

--- app/settings.py
+++ app/settings.py
@@ -44,7 +44,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # Application definition
 
 INSTALLED_APPS = [
-     'rest_framework',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
